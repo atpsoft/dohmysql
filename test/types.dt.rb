eece6a7a504b4679f56ec2aef1620ce84dc5f6d1
Makani Mason <gitauthor@pqmland.com>
2012-02-21 10:18:34 -0700

    add unit tests for type conversion in mysql2

--- test/types.dt.rb
+++ test/types.dt.rb
@@ -5,18 +5,23 @@ module DohDb
 
 class Test_types < DohTest::TestGroup
   def test_stuff
-    dbh = DohDb::request_handle
-    tbl = "doh_mysql_types_stuff_test"
-    tbl2 = "island.doh_mysql_types_stuff_another_test"
-    DohDb::query("CREATE TEMPORARY TABLE #{tbl} (amount INT)")
-    DohDb::query("INSERT INTO #{tbl} SET amount = NULL")
-    assert_equal(nil, DohDb::select_field("SELECT amount FROM #{tbl}"))
-    DohDb::query("UPDATE #{tbl} SET amount = 5")
-    assert_equal(5, DohDb::select_field("SELECT amount FROM #{tbl}"))
+    Mysql2::Client.default_query_options[:database_timezone] = :utc
+    Mysql2::Client.default_query_options[:application_timezone] = :utc
+    DohDb::connector_instance.reset
 
-    DohDb::query("CREATE TEMPORARY TABLE #{tbl2} (other INT)")
-    DohDb::query("INSERT INTO #{tbl2} SET other = 7")
-    assert_equal(5, DohDb::select_field("SELECT amount, other FROM #{tbl}, #{tbl2}"))
+    @tbl = "doh_mysql_type_conversion_tests"
+    DohDb::query("DROP TABLE IF EXISTS #@tbl")
+    DohDb::request_handle.query("CREATE TEMPORARY TABLE #{@tbl} (char_field CHAR(1) NOT NULL, int_field INT, bool_field TINYINT(1), date_field DATE, datetime_field DATETIME, decimal_field DECIMAL(7,2))")
+
+    dbh = DohDb::request_handle
+    dbh.insert("INSERT INTO #{@tbl} SET char_field = 'c', int_field = 5, bool_field = 1, date_field = '2012-02-20', datetime_field = '2012-02-20 21:06:00', decimal_field = 54.12")
+    row = dbh.select_row("SELECT * FROM #{@tbl}")
+    assert_equal('c', row['char_field'])
+    assert_equal(5, row['int_field'])
+    assert_equal(true, row['bool_field'])
+    assert_equal(Date.new(2012,2,20), row['date_field'])
+    assert_equal(Time.utc(2012,2,20, 21, 6, 0), row['datetime_field'])
+    assert_equal(BigDecimal('54.12'), row['decimal_field'])
   end
 end
 
