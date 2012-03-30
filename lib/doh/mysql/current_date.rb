a172547e62c4e4315070dbd99952c2bc67c6516a
Makani Mason <gitauthor@pqmland.com>
2012-03-29 19:52:21 -0600

    remove server_datetime, and parse since that was the last thing dependent on it

--- lib/doh/mysql/current_date.rb
+++ lib/doh/mysql/current_date.rb
@@ -1,7 +1,5 @@
 require 'doh/current_date'
 require 'doh/mysql/db_date'
-require 'doh/mysql/parse'
-require 'doh/mysql/to_sql'
 
 module DohDb
 
@@ -11,13 +9,6 @@ end
 
 def self.current_datetime_db
   Doh::current_datetime(DohDb::now)
-end
-
-def self.server_datetime
-  retval = DohDb::select_field("SELECT #{DohDb::current_datetime_db.to_sql}")
-  # if there is a fake datetime right now, will need to parse it
-  retval = DohDb::parse_datetime(retval) if retval.is_a?(String)
-  retval
 end
 
 end
