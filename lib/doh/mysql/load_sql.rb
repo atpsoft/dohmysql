2c83dea5ba8d5c6774bdcc0d1a9e2dddbc9346a8
Makani Mason <gitauthor@pqmland.com>
2012-04-12 09:53:46 -0600

    change load_sql implementation so it works with jruby

--- lib/doh/mysql/load_sql.rb
+++ lib/doh/mysql/load_sql.rb
@@ -6,14 +6,26 @@ def self.mysql_arg(value, option_specifier)
   if value.to_s.strip.empty? then '' else " -#{option_specifier}#{value}" end
 end
 
-def self.load_sql(dbconfig, filenames)
+# NOTE: this doesn't work on jruby, but keeping it around in case it's determined to be significantly faster than the more portable version in place now
+# in which case, we can use the popen version on the rubys that it works with and the other one as needed
+def self.load_sql_using_popen(dbconfig, filenames)
+  dohlog.debug("loading sql file: " + filenames.first) if filenames.size == 1
+
   mysqlcmd = 'mysql' + mysql_arg(dbconfig[:host], 'h') + mysql_arg(dbconfig[:username], 'u') + mysql_arg(dbconfig[:password], 'p') + ' ' + dbconfig[:database]
   io = IO.popen(mysqlcmd, 'r+')
-  dohlog.debug("loading sql file: " + filenames.first) if filenames.size == 1
   filenames.each do |elem|
     open(elem) {|file| io << file.read}
   end
   io.close
+end
+
+def self.load_sql(dbconfig, filenames)
+  dohlog.debug("loading sql file: " + filenames.first) if filenames.size == 1
+
+  basecmd = 'mysql' + mysql_arg(dbconfig[:host], 'h') + mysql_arg(dbconfig[:username], 'u') + mysql_arg(dbconfig[:password], 'p') + ' ' + dbconfig[:database] + ' < '
+  filenames.each do |elem|
+    `#{basecmd} #{elem}`
+  end
 end
 
 end
