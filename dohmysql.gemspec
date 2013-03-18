f0de418f351fa1d9c3ff63aaa82efcb1f1eaec79
Kem Mason <bulbous@gmail.com>
2013-03-18 15:08:33 +1300

    move options portion of migrate binary to migrate/options.rb so can be shared with a wrapper if you want.

--- dohmysql.gemspec
+++ dohmysql.gemspec
@@ -2,7 +2,7 @@ require 'rake'
 
 Gem::Specification.new do |s|
   s.name = 'dohmysql'
-  s.version = '0.2.12'
+  s.version = '0.2.13'
   s.summary = 'friendly mysql client interface'
   s.description = 'wrapper classes around low level mysql gem to provide a better interface'
   s.require_path = 'lib'
---
+++ lib/doh/mysql/migrate/options.rb
@@ -0,0 +1,41 @@
+module DohDb
+module Migrate
+COMMAND_INFO = [
+['make', '<name>', 'creates the files for a new migration'],
+['check', '<name>', 'checks that all migrations are valid'],
+['commit', '<name>', 'UNIMPLEMENTED: executes the check command, then git add,commit'],
+['apply', '<name>', 'applies one or more migrations'],
+['revert', '<name>', 'reverts one or more migrations'],
+['verify' ,'<name>', 'verifies that the migration has been integrated into the base files'],
+['merge', '<name>', 'UNIMPLEMENTED: executes the verify command, then git rm,commit'],
+].freeze
+COMMANDS = COMMAND_INFO.collect {|name, args, desc| name}.freeze
+NOTIFY_COMMANDS = %w(apply revert)
+
+cmd_detail = COMMAND_INFO.collect do |name, args, desc|
+  args = ' ' + args unless args.empty?
+  name_rpad = ' ' * (33 - (name.size + args.size))
+  "    #{name}#{args}#{name_rpad}#{desc}"
+end.join("\n")
+
+OPTS = Doh::Options.new(
+{'database' => [Doh.config[:default_database], "-d", "--database <database>", "name of the database to migrate -- defaults to config[:default_database], currently '#{Doh.config[:default_database]}'"] \
+}, true, "Commands:\n\n#{cmd_detail}")
+
+CMD_NAME, CMD_ARGS = OPTS.varargs.shift, OPTS.varargs
+unless COMMANDS.include?(CMD_NAME)
+  warn "unrecognized command #{cmd_name}"
+  exit 1
+end
+
+if OPTS.database.to_s.empty?
+  warn "You must specify a database, either here with -d or in config"
+  exit 1
+end
+
+def self.cmd_name_and_args
+  [OPTS, CMD_NAME, CMD_ARGS]
+end
+
+end
+end
